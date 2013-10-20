2c903e38d41461d88776440d677a546d4f707fc7
hoseinBL <hoseynbehkamal@gmail.com>
2013-10-20 11:54:27 +0330

    add colors

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 $(function(){
   var
-  $btns = $('.btn'),
-  colors = ['#123','#234','...'],
-  selected = null,
-  rnd = function(min,max){
-    return  Math.floor(math.random()*(max-min+1)+min);
-  },
-  setColors = function(){
+    $btns = $('.btn'),
+    colors = ['#1ABC9C','#2ECC71','#3498DB','#34495E','#9B59B6','#F1C40F','#E67E22','#E74C3C','#7F8C8D','#2980B9','#95A5A6','#C0392B','#D35400','#F39C12','#666'],
+    selected = null,
+    rnd = function(min,max){
+      return  Math.floor(Math.random()*(max-min+1)+min);
+    },
+    setColors = function(){
 
-  },
-  exploid = function(){
+    },
+    exploid = function(){
+      
+    },
+    choose1 = function(){
 
-  },
-  choose1 = function(){
-
-  },
-  setEvents = function(){
-    $btns.click(choose1);
-  },
-  init = function(){
-    setColors();
-    exploid();
-    setEvents();
-  };
+    },
+    setEvents = function(){
+      $btns.click(choose1);
+    },
+    init = function(){
+      setColors();
+      exploid();
+      setEvents();
+    };
 
   init();
 });
