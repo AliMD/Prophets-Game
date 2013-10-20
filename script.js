a461989879643863053b13fb5c1b40526be172b8
Ali Mihandoost <i@ali.md>
2013-10-20 12:18:03 +0330

    Add start and reset fn

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 $(function(){
   var
-    $btns = $('.btn'),
-    colors = ['#1ABC9C','#2ECC71','#3498DB','#34495E','#9B59B6','#F1C40F','#E67E22','#E74C3C','#7F8C8D','#2980B9','#95A5A6','#C0392B','#D35400','#F39C12','#666'],
-    selected = null,
-    rnd = function(min,max){
-      return  Math.floor(Math.random()*(max-min+1)+min);
-    },
-    setColors = function(){
-      $btns.each(function(){
-        $(this).css({
-          'background-color': colors[rnd(0,14)]
-        });
+  $start = $('#start'),
+  $btns = $('.btns .btn'),
+  colors = ['#1ABC9C','#2ECC71','#3498DB','#34495E','#9B59B6','#F1C40F','#E67E22','#E74C3C','#7F8C8D','#2980B9','#95A5A6','#C0392B','#D35400','#F39C12','#666'],
+  selected = null,
+  rnd = function(min,max){
+    return  Math.floor(Math.random()*(max-min+1)+min);
+  },
+  randColor = function(){
+    return colors[rnd(0,colors.length-1)];
+  },
+  setColors = function(){
+    $btns.each(function(){
+      $(this).css({
+        'background-color': randColor()
       });
-    },
-    exploid = function(){
-      $btns.each(function(){
-        $(this).css({
-          left : rnd(0,90) + '%',
-          top : rnd(0,90) + '%'
-        });
-      });
-    },
-    choose1 = function(){
+    });
+  },
+  exploid = function(){
+    $btns.each(function(){
+      $(this).animate({
+        left : rnd(0,90) + '%',
+        top : rnd(0,90) + '%'
+      },400);
+    });
+  },
+  choose1 = function(){
 
-    },
-    setEvents = function(){
-      $btns.click(choose1);
-    },
-    init = function(){
-      setColors();
-      exploid();
-      setEvents();
-    };
+  },
+  start = function(){
+    exploid();
+    $btns.click(choose1);
+  },
+  reset = function(){
+    window.location.reload(); // cheap as posible !
+  },
+  init = function(){
+    $start.click(start);
+    setColors();
+  };
 
   init();
+
 });
